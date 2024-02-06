fc75f3f5e454509a811c90b81cf41c5b696a3d9b
Faessal Sufi <faessalsufi@gmail.com>
2024-02-07 00:04:21 +0530

    Login, Select, Signup, PostCard Components added and Auth Layout file added

---
+++ Blog project/src/components/Select.jsx
@@ -0,0 +1,25 @@
+import React, { useId } from "react";
+import { forwardRef } from "react";
+
+function Select({ options, label, className, ...props }, ref) {
+  const id = useId();
+
+  return (
+    <div className="w-full">
+      {label && (
+        <label htmlFor={id} className="">
+          {label}
+        </label>
+      )}
+      <select id={id} {...props} ref={ref} className={`${className}`}>
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default React.forwardRef(Select);
